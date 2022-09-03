063080c1d6c5d2e796614c49de0accbdbd087415
brightvarghese <77091625+brightvarghese@users.noreply.github.com>
2022-09-03 13:13:09 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+
+class Stack:
+  def __init__(self):
+    self.head = None
+
+  def push(self, data) -> None:
+    # Write your code here
+
+  def pop(self) -> None:
+    # Write your code here
+
+  def status(self):
+    """
+    It prints all the elements of stack.
+    """
+    # Write your code here  
+
+
+# Do not change the following code
+stack = Stack()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "push":
+    stack.push(int(data[i]))
+  elif operations[i] == "pop":
+    stack.pop()
+stack.status()
